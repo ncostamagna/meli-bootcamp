b827de5a1929810b4488b099f6d17dff5504d700
Nahuel Costamagna <nlcostamagna@gmail.com>
2021-07-08 23:13:34 -0300

    feat: add mock functionality in storage pkg

--- pkg/store/file.go
+++ pkg/store/file.go
@@ -8,6 +8,8 @@ import (
 type Store interface {
 	Read(data interface{}) error
 	Write(data interface{}) error
+	AddMock(mock *Mock)
+	ClearMock()
 }
 
 type Type string
@@ -19,16 +21,37 @@ const (
 func New(store Type, fileName string) Store {
 	switch store {
 	case FileType:
-		return &FileStore{fileName}
+		return &FileStore{fileName, nil}
 	}
 	return nil
 }
 
 type FileStore struct {
 	FileName string
+	Mock *Mock
+}
+
+type Mock struct {
+	Data interface{}
+	Err error
+}
+
+func (fl *FileStore) AddMock(mock *Mock) {
+	fl.Mock = mock
+}
+
+func (fl *FileStore) ClearMock() {
+	fl.Mock = nil
 }
 
 func (fs *FileStore) Write(data interface{}) error {
+	if fs.Mock != nil {
+		if fs.Mock.Err != nil {
+			return fs.Mock.Err
+		}
+		return nil
+	}
+
 	file, err := json.MarshalIndent(data, "", " ")
 	if err != nil {
 		return err
@@ -37,6 +60,16 @@ func (fs *FileStore) Write(data interface{}) error {
 }
 
 func (fs *FileStore) Read(data interface{}) error {
+
+	if fs.Mock != nil {
+		if fs.Mock.Err != nil {
+			return fs.Mock.Err
+		}
+		data = fs.Mock.Data
+		return nil
+	}
+
+
 	file, err := ioutil.ReadFile(fs.FileName)
 	if err != nil {
 		return err
