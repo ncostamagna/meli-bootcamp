98ac2b4d4fd41141f8e308719f89322a8d0ff4b5
Nahuel Costamagna <nlcostamagna@gmail.com>
2021-07-16 05:34:38 -0300

    test: add product functional test

--- go.mod
+++ go.mod
@@ -17,4 +17,5 @@ require (
 	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	golang.org/x/tools v0.1.4 // indirect
+	github.com/stretchr/testify v1.7.0
 )
---
+++ test/product_test.go
@@ -0,0 +1,79 @@
+package test
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/ncostamagna/meli-bootcamp/cmd/server/handler"
+	"github.com/ncostamagna/meli-bootcamp/internal/products"
+	"github.com/ncostamagna/meli-bootcamp/pkg/store"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func createServer() *gin.Engine {
+	_ = os.Setenv("TOKEN", "123456")
+	db := store.New(store.FileType, "../products.json")
+	repo := products.NewRepository(db)
+	service := products.NewService(repo)
+	p := handler.NewProduct(service)
+
+	r := gin.Default()
+
+	pr := r.Group("/products")
+	pr.POST("/", p.Store())
+	pr.GET("/", p.GetAll())
+	return r
+}
+
+func createRequestTest(method string, url string, body string) (*http.Request, *httptest.ResponseRecorder) {
+	req := httptest.NewRequest(method, url, bytes.NewBuffer([]byte(body)))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("token", "123456")
+
+	return req, httptest.NewRecorder()
+}
+
+
+
+func Test_SaveProduct_OK(t *testing.T) {
+	objReq := struct{
+		Code string           `json:"code"`
+		Data products.Product `json:"data"`
+	}{}
+	r := createServer()
+
+	req, rr := createRequestTest(http.MethodPost, "/products/", `{
+        "nombre": "Tester","tipo": "Funcional","cantidad": 10,"precio": 99.99
+    }`)
+	r.ServeHTTP(rr, req)
+	assert.Equal(t, 200, rr.Code)
+
+	err := json.Unmarshal(rr.Body.Bytes(), &objReq)
+	assert.Nil(t, err)
+	assert.Equal(t, objReq.Code, "200")
+	assert.Equal(t, objReq.Data.Price, 99.99)
+	assert.Equal(t, objReq.Data.Count, 10)
+	assert.Equal(t, objReq.Data.Type, "Funcional")
+	assert.Equal(t, objReq.Data.Name, "Tester")
+}
+
+func Test_GetProduct_OK(t *testing.T) {
+	objReq := struct{
+		Code  string 			 `json:"code"`
+		Data  []products.Product `json:"data"`
+	}{}
+	r := createServer()
+
+	req, rr := createRequestTest(http.MethodGet, "/products/", "")
+	r.ServeHTTP(rr, req)
+	assert.Equal(t, 200, rr.Code)
+
+	err := json.Unmarshal(rr.Body.Bytes(), &objReq)
+	assert.Nil(t, err)
+	assert.Equal(t, objReq.Code, "200")
+	assert.Equal(t, len(objReq.Data) > 0, true)
+}
